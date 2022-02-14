a7c6471af766226af765e421e37c9466cb6b50f6
sharangkalra01 <97080262+sharangkalra01@users.noreply.github.com>
2022-02-14 18:16:57 +0530

    Add files via upload

---
+++ 21103012_Assign03_output.py
@@ -0,0 +1,70 @@
+''' By = SHARANG KALRA
+    SID= 21103012
+'''
+
+#ASSIGNMENT3
+#QUESTION1 OUTPUT
+
+ENTER YOUR VALUE: sharang
+{'s': 1, 'h': 1, 'a': 2, 'r': 1, 'n': 1, 'g': 1}
+
+#QUESTION2 OUTPUT
+
+ENTER THE MONTH OF THE YEAR :02
+enter the year :2022
+enter the day :28
+Date- 28 / 2 / 2022
+Next Date : 1/ 3 / 2022
+
+#QUESTION3 OUTPUT
+
+Enter elements of a list : 3 9 10
+list:  ['3', '9', '10']
+The tuples list with number and it's square is : [(3, 9), (9, 81), (10, 100)]
+
+#QUESTION4 OUTPUT
+
+Give your grade points ranging from 4 to 10 :6
+Your Grade is 'C+' and Average Performance
+
+#QUESTION5 OUTPUT
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+#QUESTION6 OUTPUT
+
+Please enter your SID : 21103012
+Please enter your name :sharang kalra
+Do you want to enter another student entry, Please answer in Y or N only :y
+Enter SID: 21103025
+Enter Name: akash chopra
+Do you want to enter another student entry, Please answer in Y or N only :n
+{21103012: 'sharang kalra', 21103025: 'akash chopra'}
+{21103025: 'akash chopra', 21103012: 'sharang kalra'}
+{21103012: 'sharang kalra', 21103025: 'akash chopra'}
+Search Name with SID: 21103012
+sharang kalra
+
+#QUESTION7 OUTPUT
+
+ENTER THE NUMBER OF TERMS IN THE SERIES: 5
+Fibonacci sequence:
+0
+1
+1
+2
+3
+The average is : 1.4
+
+#QUESTION8 OUTPUT
+
+{1, 3, 5, 6, 8}
+{3, 6, 8, 9, 13, 17}
+{1, 2, 4, 5}
+{6, 7, 8, 9, 10}
+{10, 7}
